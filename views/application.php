895a7f82c721752844c1134b750804adacf9ac2a
Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-08-10 09:36:06 +0700

    feat: basic header, footer by boostrap, font-awesome

--- views/application.php
+++ views/application.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"
+    />
+
     <link rel="stylesheet" href="/assets/css/style.css">
     <title>PHP MVC Sample</title>
 </head>
--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,3 +1,20 @@
-<?php
-
-echo('footer session');
+<div class="container-fluid bg-primary">
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About</a></li>
+    </ul>
+    <ul class="nav justify-content-center">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-white"><i class="fab fa-facebook"></i></a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-white"><i class="fab fa-instagram"></i></a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-white"><i class="fab fa-twitter"></i></a></li>
+    </ul>
+    
+    <p class="text-center text-white">© 2021 HCMUS, Inc</p>
+  </footer>
+</div>
+</div>
